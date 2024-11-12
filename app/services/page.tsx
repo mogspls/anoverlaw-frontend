b39a7f44cec301724d693f18787ee9a4964f944f
Mogi Añover <mogianover@192.168.68.113>
2024-11-12 14:27:54 +0800

    Services bulleted list

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -47,25 +47,35 @@ export default async function Services() {
           </section>
           <section className="grid grid-cols-2 lg:grid-cols-3 gap-1">
             {posts.data.map((post: Post, key: number) => (
-              <a
-                href={`/services/${post.attributes.slug}`}
-                className="flex justify-between w-full hover:bg-[#1B387D] duration-75 bg-black p-4 text-white items-end h-56"
-                key={key}
-              >
-                <h2 className="text-sm lg:text-lg">{post.attributes.name}</h2>
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 1000 1000"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="#fff"
+              // <a
+              //   href={`/services/${post.attributes.slug}`}
+              //   className="flex justify-between w-full hover:bg-[#1B387D] duration-75 bg-black p-4 text-white items-end h-56"
+              //   key={key}
+              // >
+              //   <h2 className="text-sm lg:text-lg">{post.attributes.name}</h2>
+              //   <svg
+              //     width="24"
+              //     height="24"
+              //     viewBox="0 0 1000 1000"
+              //     xmlns="http://www.w3.org/2000/svg"
+              //     fill="#fff"
+              //   >
+              //     <path
+              //       d=" M 165 565C 165 565 465 265 465 265C 484 245 516 245 535 265C 535 265 835 565 835 565C 855 584 856 616 836 636C 816 656 784 655 765 635C 765 635 500 371 500 371C 500 371 235 635 235 635C 221 650 200 655 181 647C 162 639 150 621 150 601C 150 587 155 574 165 565C 165 565 165 565 165 565"
+              //       transform="rotate(90,500,500)"
+              //     />
+              //   </svg>
+              // </a>
+
+              <li key={key} className="py-4 px-2 ml-4">
+                <a
+                  href={`/services/${post.attributes.slug}`}
+                  className="hover:underline spectral sc"
+                  key={key}
                 >
-                  <path
-                    d=" M 165 565C 165 565 465 265 465 265C 484 245 516 245 535 265C 535 265 835 565 835 565C 855 584 856 616 836 636C 816 656 784 655 765 635C 765 635 500 371 500 371C 500 371 235 635 235 635C 221 650 200 655 181 647C 162 639 150 621 150 601C 150 587 155 574 165 565C 165 565 165 565 165 565"
-                    transform="rotate(90,500,500)"
-                  />
-                </svg>
-              </a>
+                  {post.attributes.name}
+                </a>
+              </li>
             ))}
           </section>
         </div>
